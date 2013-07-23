452c6e1792caf3e6569c086859cd27da6c1f3776
Konrad Mrożek <konrad.mrozek@gmail.com>
2013-07-23 21:01:25 +0200

    SwingPlayerPresenter TO DO

---
+++ src/main/java/com/mrroman/springgames/service/SwingPlayerPresenter.java
@@ -0,0 +1,18 @@
+package com.mrroman.springgames.service;
+
+import com.mrroman.springgames.domain.Player;
+
+/**
+ * To present player use gravatar service:
+ * 
+ *  http://www.gravatar.com/avatar/MD5_HASH_OF_EMAIL
+ * 
+ */
+public class SwingPlayerPresenter implements PlayerPresenter {
+
+	@Override
+	public void present(Player player) {
+		// TODO
+	}
+
+}
