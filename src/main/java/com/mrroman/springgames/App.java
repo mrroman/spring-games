ce37e4cd6862fe42891a961749f92ea2e2ce8f88
Konrad Mrożek <konrad.mrozek@gmail.com>
2013-07-23 20:32:30 +0200

    Add player presenter service interface.

--- src/main/java/com/mrroman/springgames/App.java
+++ src/main/java/com/mrroman/springgames/App.java
@@ -1,9 +1,28 @@
 package com.mrroman.springgames;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		ApplicationContext applCtx = fromXml();
+//		ApplicationContext applCtx = fromAnnotations();
+	
+		applCtx.
+	}
+	
+	
+
+	private static ApplicationContext fromAnnotations() {
+		 AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext();
+		 annotationConfigApplicationContext.register(Config.class);
+		 return annotationConfigApplicationContext;
+	}
+
+	private static ApplicationContext fromXml() {
+		return new ClassPathXmlApplicationContext("applicationContext.xml");
 	}
 	
 }
---
+++ src/main/java/com/mrroman/springgames/service/PlayerPresenter.java
@@ -0,0 +1,9 @@
+package com.mrroman.springgames.service;
+
+import com.mrroman.springgames.domain.Player;
+
+public interface PlayerPresenter {
+
+	void present(Player player);
+	
+}
